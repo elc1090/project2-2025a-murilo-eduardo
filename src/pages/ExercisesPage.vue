5c9e8e78af869fa1c15ec1f81a38e592d77a0bf1
Eduardo da Silveira <essylveira@gmail.com>
2025-04-26 21:24:36 -0300

    feat: implement creation of trainings given a routine

--- src/pages/ExercisesPage.vue
+++ src/pages/ExercisesPage.vue
@@ -1,12 +1,29 @@
 <template>
-  <div class="flex flex-col items-center justify-center min-h-screen p-8">
-    <div>
+  <div class="p-8">
+    <q-select
+      class="pb-4"
+      color="blue-12"
+      popup-content-class="text-black"
+      filled
+      v-model="selectedRoutine"
+      :options="routines.map((routine) => routine.name)"
+      label="Treino"
+    />
+    <q-select
+      class="pb-4"
+      color="blue-12"
+      popup-content-class="text-black"
+      filled
+      v-model="selectedDay"
+      :options="days.map((day) => day.name)"
+      label="Dia"
+    />
+    <div class="pb-4">
       <q-btn
-        class="fixed right-4 z-[50]"
         label="Montar Treino"
         icon="edit"
         color="light-blue"
-        @click="() => console.log('Not implemented.')"
+        @click="mount"
         no-caps
         push
       >
@@ -14,6 +31,21 @@
       </q-btn>
       <span class="text-white text-2xl font-bold">{{ exerciseCount }}</span>
     </div>
+    <q-btn
+      label="Debug"
+      icon="edit"
+      color="negative"
+      @click="
+        async () => {
+          await deleteRoutines();
+          await createRoutine('Treino Teste');
+        }
+      "
+      no-caps
+      push
+    ></q-btn>
+  </div>
+  <div class="flex flex-col items-center justify-center min-h-screen p-8">
     <q-list>
       <q-item
         class="bg-primary rounded-xl q-pa-md q-mb-md"
@@ -68,8 +100,17 @@
   </div>
 </template>
 <script setup>
-import { ref, onMounted, computed } from "vue";
-import { getData, getDetails } from "src/services/WgerService";
+import { ref, watch, onMounted, computed } from "vue";
+import {
+  createRoutine,
+  createSlotEntry,
+  getRoutines,
+  deleteRoutines,
+  getSlots,
+  getDays,
+  getData,
+  getDetails,
+} from "src/services/WgerService";
 import { useQuasar } from "quasar";
 import { useExerciseStorage } from "src/stores/exerciseStorage";
 
@@ -80,7 +121,6 @@ const fetchExercises = async () => {
   $q.loading.show();
   try {
     const { data } = await getData("exerciseinfo");
-    console.log("🚀 ~ fetchExercises ~ data:", data);
     if (data) exercises.value = data.results;
   } catch (error) {
     console.log("🚀 ~ fetchExercises ~ error:", error);
@@ -94,6 +134,7 @@ const fetchExercises = async () => {
   }
 };
 
+// Exercise storage related
 const exerciseStorage = useExerciseStorage();
 
 const addExercise = (id) => {
@@ -109,7 +150,46 @@ const exerciseCount = computed(() => {
   return exerciseStorage.data.length;
 });
 
+// Routine related
+const selectedRoutine = ref(null);
+const routines = ref([]);
+const fetchRoutines = async () => {
+  routines.value = await getRoutines();
+};
+
+// Day related
+const selectedDay = ref(null);
+const days = ref([]);
+watch(selectedRoutine, async (routineName) => {
+  if (routineName) {
+    let routineIndex = routines.value.findIndex(
+      (routine) => routineName == routine.name
+    );
+    let routine = routines.value[routineIndex];
+    days.value = await getDays(routine.id);
+  } else {
+    days.value = [];
+  }
+});
+
+const mount = async () => {
+  let dayIndex = days.value.findIndex((day) => day.name == selectedDay.value);
+  let day = days.value[dayIndex];
+
+  let slots = await getSlots(day.id);
+
+  // There should be one and only one slot.
+  let slot = slots[0].id;
+
+  exerciseStorage.data.forEach((exercise) => {
+    createSlotEntry(slot, exercise);
+  });
+
+  exerciseStorage.save([]);
+};
+
 onMounted(() => {
   fetchExercises();
+  fetchRoutines();
 });
 </script>
--- src/services/WgerService.js
+++ src/services/WgerService.js
@@ -7,3 +7,114 @@ export async function getData(type, params) {
 export async function getDetails(type, id, params) {
   return axiosService.get(`/${type}/${id}/`, { params: { params } });
 }
+
+export async function getRoutines() {
+  const routines = await axiosService.get("/routine/");
+  return routines.data.results;
+}
+
+export async function getDays(routine) {
+  let days = await axiosService.get("/day/");
+  return days.data.results.filter((day) => day.routine === routine);
+}
+
+export async function getSlots(day) {
+  let slots = await axiosService.get("/slot/");
+  console.log(slots.data.results);
+  return slots.data.results.filter((slot) => slot.day == day);
+}
+
+export async function getSlotEntries(slot) {
+  let slotEntries = await axiosService.get("/slot-entry/");
+  return slotEntries.data.results.filter((slotEntry) => slotEntry.slot == slot);
+}
+
+export async function createRoutine(name) {
+  const body = {
+    name: name,
+    // description: "string",
+    start: "2025-01-01",
+    end: "2025-12-31",
+    // fit_in_week: true,
+    // is_template: true,
+    // is_public: true,
+  };
+
+  let response = await axiosService.post("/routine/", body);
+  let routine = response.data;
+
+  const weekDays = [
+    "Segunda",
+    "Terça",
+    "Quarta",
+    "Quinta",
+    "Sexta",
+    "Sábado",
+    "Domingo",
+  ];
+
+  weekDays.forEach(async (weekDay) => {
+    await createDay(routine.id, weekDay);
+  });
+
+  return routine;
+}
+
+export async function createDay(routine, name) {
+  const body = {
+    routine: routine,
+    // "order": 2147483647,
+    name: name,
+    // "description": "string",
+    // "is_rest": true,
+    // "need_logs_to_advance": true,
+    // "type": "custom",
+    // "config": "string"
+  };
+
+  let response = await axiosService.post("/day/", body);
+  let day = response.data;
+
+  await createSlot(day.id);
+
+  return day;
+}
+
+export async function createSlot(day) {
+  const body = {
+    day: day,
+    // order: 2147483647,
+    // comment: "string",
+    // config: "string",
+  };
+
+  let response = await axiosService.post("/slot/", body);
+
+  return response.data;
+}
+
+export async function createSlotEntry(slot, exercise) {
+  const body = {
+    slot: slot,
+    exercise: exercise,
+    // type: "normal",
+    // repetition_unit: 0,
+    // repetition_rounding: "54.06",
+    // weight_unit: 0,
+    // weight_rounding: "-91",
+    order: 2147483647, // If order is not specified, the API doesn't work.
+    // comment: "string",
+    // config: "string",
+  };
+
+  let response = await axiosService.post("/slot-entry/", body);
+  return response.data;
+}
+
+export async function deleteRoutines() {
+  let routines = await getRoutines();
+
+  routines.map((routine) => {
+    axiosService.delete(`/routine/${routine.id}`);
+  });
+}
